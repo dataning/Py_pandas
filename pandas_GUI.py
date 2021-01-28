ef833fe6336f4d666076552bab979645b6dcb7fe
Lawrence Lu <datalulu@gmail.com>
2021-01-28 19:06:30 +0000

    1

--- pandas_GUI.py
+++ pandas_GUI.py
@@ -1,9 +1,13 @@
+# Load packages
 import pandas as pd
-
 from pandasgui import show
+
+# Datasets
 df = pd.DataFrame(([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=['a', 'b', 'c'])
 show(df)
 
-
+# Download all the sample datasets
 from pandasgui.datasets import pokemon, titanic, all_datasets
+
+# Show all the datasets
 show(**all_datasets)
