cacabdd80dcd984338d2a4f61479909afe985ace
Lawrence Lu <datalulu@gmail.com>
2021-01-28 19:01:40 +0000

    1

---
+++ pandas_GUI.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+from pandasgui import show
+df = pd.DataFrame(([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=['a', 'b', 'c'])
+show(df)
+
+
+from pandasgui.datasets import pokemon, titanic, all_datasets
+show(**all_datasets)
